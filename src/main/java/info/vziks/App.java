c3537b58d13d5665f145aa26552e2f73620c29e0
vziks <vziks@live.ru>
2021-03-11 14:38:24 +0300

    +

--- src/main/java/info/vziks/App.java
+++ src/main/java/info/vziks/App.java
@@ -1,13 +1,16 @@
 package info.vziks;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(8080).addService(new HelloServiceImpl()).build();
+        server.start();
+        System.out.println("Start gRPC server");
+        server.awaitTermination();
     }
 }
--- src/main/java/info/vziks/HelloServiceImpl.java
+++ src/main/java/info/vziks/HelloServiceImpl.java
@@ -1,7 +1,25 @@
-package info.vziks;/**
+package info.vziks;
+
+import info.vziks.grpc.AppServiceGrpc;
+import info.vziks.grpc.AppServiceOuterClass;
+import io.grpc.stub.StreamObserver;
+
+/**
  * Class HelloServiceImpl
  * Project grpcdemo
  *
  * @author Anton Prokhorov <vziks@live.ru>
- */public class HelloServiceImpl {
+ */
+public class HelloServiceImpl extends AppServiceGrpc.AppServiceImplBase {
+
+    @Override
+    public void hello(AppServiceOuterClass.AppRequest request, StreamObserver<AppServiceOuterClass.AppResponse> responseObserver) {
+
+        System.out.println(request);
+        AppServiceOuterClass.AppResponse response = AppServiceOuterClass.AppResponse
+                .newBuilder()
+                .setHello("Hello server " + request.getName()).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
