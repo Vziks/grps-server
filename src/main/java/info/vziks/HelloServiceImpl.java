ce0dcb25d75a3e81982fe920e1fff815e581331f
vziks <vziks@live.ru>
2021-03-11 15:21:03 +0300

    +

--- src/main/java/info/vziks/HelloServiceImpl.java
+++ src/main/java/info/vziks/HelloServiceImpl.java
@@ -15,12 +15,23 @@ public class HelloServiceImpl extends AppServiceGrpc.AppServiceImplBase {
     @Override
     public void hello(AppServiceOuterClass.AppRequest request, StreamObserver<AppServiceOuterClass.AppResponse> responseObserver) {
 
-        System.out.println(request);
-        AppServiceOuterClass.AppResponse response = AppServiceOuterClass.AppResponse
-                .newBuilder()
-                .setHello("Hello server " + request.getName() + " Arr" + request.getArrList())
-                .build();
-        responseObserver.onNext(response);
+
+        for (int i = 0; i < 1000; i++) {
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(request);
+            AppServiceOuterClass.AppResponse response = AppServiceOuterClass.AppResponse
+                    .newBuilder()
+                    .setHello("Hello server " + request.getName() + " Arr" + request.getArrList())
+                    .build();
+            responseObserver.onNext(response);
+        }
+
         responseObserver.onCompleted();
     }
 }
